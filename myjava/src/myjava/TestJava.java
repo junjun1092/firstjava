47fbd5c50c926c924dc387cdf31127fc36288b69
zhaojunjun <zhaojunjun0531@126.com>
2016-06-01 22:19:30 +0800

    First Commit.

---
+++ myjava/src/myjava/TestJava.java
@@ -0,0 +1,11 @@
+package myjava;
+
+public class TestJava 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Wolrd");
+	}
+	//https://github.com/junjun1092/firstjava.git
+
+}
